feb8d35ee6b4d78a8df200d6a745b5752b8fc29f
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-17 23:15:15 +0000

    Characters

--- variables.c
+++ variables.c
@@ -2,14 +2,13 @@
 int main()
 {
     //variable = a reusable container
-    double pi = 3.141592653;
-    double e = 2.7182818284590;
-    /*doubles use the format specifier %lf(long float) and usually have 6 digits after decimal
-    float precision is limited to 6 digits
-    double precision is limited to 16 digits
+    char grade = 'A';
+    /*
+    Characters take single quotes 
+    they have a format specifier %c
     */
-    printf("The value of pi is %lf\n", pi);
-    printf("The value of e is %.12lf\n", e);
-
+   printf("Grade: %c\n",grade);
+   printf("Excellent!\n");
+   
     return 0;
 }
