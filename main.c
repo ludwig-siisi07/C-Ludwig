71b53c719f8e7f9fe9e53d3d93b40e0661e29998
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-17 22:26:24 +0000

    1st C code

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    // This is my first program :)
+    printf("I love watching football!\n");
+    printf("I am a big fan of liverpool football club");
+    
+    return 0;
+}
