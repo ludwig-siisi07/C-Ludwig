1e9c56b032682351bc6415dc1621c69cb51e64b3
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-17 23:10:43 +0000

    Doubles

--- variables.c
+++ variables.c
@@ -2,10 +2,14 @@
 int main()
 {
     //variable = a reusable container
-    float gpa = 3.7;
-    //floats use the format specifier %f and usually have 6 digits after decimal
-    printf("Your gpa is %.2f\n",gpa);
-    printf("Excellent!\n");
+    double pi = 3.141592653;
+    double e = 2.7182818284590;
+    /*doubles use the format specifier %lf(long float) and usually have 6 digits after decimal
+    float precision is limited to 6 digits
+    double precision is limited to 16 digits
+    */
+    printf("The value of pi is %lf\n", pi);
+    printf("The value of e is %.12lf\n", e);
 
     return 0;
 }
