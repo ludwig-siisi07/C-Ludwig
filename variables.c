96d0b9a74d42890abe2cd0187e9983c66bd62ac8
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-17 23:27:57 +0000

    Booleans

--- variables.c
+++ variables.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
+#include<stdbool.h>
+
 int main()
 {
     //variable = a reusable container
-    /*Strings = array of characters
-    uses double quotes
-    format specifier - %s
+    /*Booleans have special header files
+    true =1 and false = 0
+    used internally with programs like if statements
     */
-   char name [] = "Ludwig Siisi";
-   printf("Hello, %s!\n",name);
-
+   bool isonline =0;
+   if(isonline){
+       printf("User is Online\n");
+   }
+   else{
+    printf("User is Offline");
+   }
     return 0;
 }
