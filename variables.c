7eb15bade5292016af9310765f7fd5f7a48435d1
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-17 23:02:26 +0000

    Floats

--- variables.c
+++ variables.c
@@ -2,8 +2,10 @@
 int main()
 {
     //variable = a reusable container
-    int age =25;
-    //%d is a format specifier
-    printf("You are %d years old", age);
+    float gpa = 3.7;
+    //floats use the format specifier %f and usually have 6 digits after decimal
+    printf("Your gpa is %.2f\n",gpa);
+    printf("Excellent!\n");
+
     return 0;
 }
