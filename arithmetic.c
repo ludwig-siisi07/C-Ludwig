dce433fb7e2f6c7ea74a3e494dde03984c330499
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-18 00:08:16 +0000

    Arithmetic

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    //arithmetic operations (+,*,-,%,/)
+    int x = 5;
+    int y = 3;
+    int z = 0; //z stores the result
+    int z2 = 0;
+    
+    z2 = 5*2;
+    z = y/x;
+    printf("%d",z);
+    // output is zero because integers can't store decimals
+
+    return 0;
+}
