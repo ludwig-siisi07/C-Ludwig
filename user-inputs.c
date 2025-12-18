754246e1753b929ad188c1560160296444a5fe93
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-18 00:22:09 +0000

    Undefined behaviour

---
+++ user-inputs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    //declaring without values creates undefined behaviour
+    int age;
+    float gpa;
+    char grade;
+    char name[30];
+
+    printf("%d\n", age);
+        printf("%f\n", gpa);
+
+
+    return 0;
+}
