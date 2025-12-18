1859b7cd9e1d31a7269b395abe543b4d21eec78a
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-18 10:47:03 +0000

    InputBuffers & scanf

--- user-inputs.c
+++ user-inputs.c
@@ -7,8 +7,21 @@ int main()
     char grade = '\0'; //null terminator
     char name[30] = "";
 
+    printf("Enter your age: ");
+    scanf("%d", &age); //&gives the location
+
+    printf("Enter your gpa: ");
+    scanf("%f", &gpa);
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+    /*%c unlike the d,f and s doesn't ignore whitespaces in buffer!
+    the leftover \n from buffer can be cleared with space before %c
+    */
     printf("%d\n", age);
-        printf("%f\n", gpa);
+    printf("%.2f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
 
 
     return 0;
