449267a1f2f682e918064e45034f364ea72abb23
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-23 11:56:14 +0000

    Nested conditionals

--- conditionals.c
+++ conditionals.c
@@ -1,26 +1,44 @@
 #include<stdio.h>
+
 int main()
 {
-    //Switch cases - alternative to multiple if-else
-    int dayofweek =0;
-    printf("Enter a number from 1-3: ");
-    scanf("%d", &dayofweek);
+    //Conditionals with booleans
+    float ticket_price = 10.00;
+    int is_student = 0;
+    int is_senior = 0;
     
-    switch(dayofweek){
-        case 1:
-            printf("It is Monday");
-            break;
-        case 2:
-            printf("It is Tuesday");
-            break;
-        case 3:
-            printf("It is Wednesday");
-            break;
-        default:
-            printf("Enter a valid input!");
+
+    /* DISCOUNT PRICES
+        Students only = 10%
+        Seniors only(alumni incl.)= 20%
+        Student + Senior = 30%
+    */
+    
+    //USER INPUTS
+    printf("Are you student?(if Yes, enter 1): ");
+    scanf("%d",&is_student);
+    printf("Are you a senior?(If yes, enter 1): ");
+    scanf("%d",&is_senior);
+
+    //CONDITIONAL CODE
+    if(is_student == 1){
+        ticket_price = 0.9*ticket_price;
+        printf("You get a student discount of 10 percent\n");
+        if (is_senior== 1){
+            ticket_price = 0.778 * ticket_price;
+            printf("You get a student discount of 20 percent\n");
+        printf("The price of your ticket is $%.2f\n", ticket_price);
+        }
     }
 
-
+    else if(is_senior == 1){
+        ticket_price *= 0.8;
+        printf("You get a student discount of 20 percent\n");
+        printf("The price of your ticket is $%.2f\n", ticket_price);
+    }
+    else{
+        printf("SORRY! Only seniors and students are allowed to purchase tickets");
+    }
 
     return 0;
 }
