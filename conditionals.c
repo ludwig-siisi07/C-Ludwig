af26d3a0449fe7e817151b9ea1304e2732cd4ba7
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-25 11:14:05 +0000

    LogicalOperators_notes

--- conditionals.c
+++ conditionals.c
@@ -2,43 +2,16 @@
 
 int main()
 {
-    //Conditionals with booleans
-    float ticket_price = 10.00;
-    int is_student = 0;
-    int is_senior = 0;
-    
+    /* 
+    LOGICAL OPERATORS IN C: &&  || !   
 
-    /* DISCOUNT PRICES
-        Students only = 10%
-        Seniors only(alumni incl.)= 20%
-        Student + Senior = 30%
+    Short-circuit evaluation:
+    - A && B : if A is false, B is not evaluated
+    - A || B : if A is true, B is not evaluated
+
+    Operator precedence:
+    1. !  2. &&  3. ||
     */
-    
-    //USER INPUTS
-    printf("Are you student?(if Yes, enter 1): ");
-    scanf("%d",&is_student);
-    printf("Are you a senior?(If yes, enter 1): ");
-    scanf("%d",&is_senior);
-
-    //CONDITIONAL CODE
-    if(is_student == 1){
-        ticket_price = 0.9*ticket_price;
-        printf("You get a student discount of 10 percent\n");
-        if (is_senior== 1){
-            ticket_price = 0.778 * ticket_price;
-            printf("You get a student discount of 20 percent\n");
-        printf("The price of your ticket is $%.2f\n", ticket_price);
-        }
-    }
-
-    else if(is_senior == 1){
-        ticket_price *= 0.8;
-        printf("You get a student discount of 20 percent\n");
-        printf("The price of your ticket is $%.2f\n", ticket_price);
-    }
-    else{
-        printf("SORRY! Only seniors and students are allowed to purchase tickets");
-    }
 
     return 0;
 }
