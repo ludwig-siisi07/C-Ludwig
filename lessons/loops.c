d600bed82128c5da0a0e36ee72fd917a5d263e8f
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2026-01-05 16:16:26 +0000

    Symbol-grid

--- lessons/loops.c
+++ lessons/loops.c
@@ -2,15 +2,29 @@
 
 int main()
 {
-    //NESTED LOOPS
-    //Multiplication table
-    for(int i = 1; i <=10; i++){
-      for (int j = 1; j <= 10; j++)
+    //SYMBOL GRID WITH NESTED LOOPS
+    int row = 0;
+    int column = 0;
+    char symbol = '\0';
+
+    printf("Enter the number of rows: ");
+    scanf("%d",&row);
+
+    printf("Enter the number of columns: ");
+    scanf("%d",&column);
+
+    printf("Enter the symbol: ");
+    scanf(" %c", &symbol);
+
+    for (int i = 1; i <= row; i++)
+    {
+      for (int j = 1; j <= column; j++)
       {
-        printf("%3d ",i*j);
+        printf("%5c",symbol);
       }
       printf("\n");
     }
+    
 
     return 0;
 }
