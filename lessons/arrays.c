af16f9b25647dbf8f30545e989590a4ef06c2544
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2026-01-19 07:40:08 +0000

    array_inputs

--- lessons/arrays.c
+++ lessons/arrays.c
@@ -3,13 +3,15 @@
 int main()
 {
     //array = variable w more than one value 
-    int numbers[] = {10, 20 ,30, 40, 50, 60};
-    int size = sizeof(numbers)/sizeof(numbers[0]);
-    //sizeof returns the amount of memory taken
-    for (int i = 0; i < size; i++)
+    int scores[5] = {0};//5 indicates number of array elements 
+    for (int i = 0; i < 5; i++)
     {
-       printf("%d ", numbers[i]);
+        printf("Enter a score: ");
+        scanf("%d",&scores[i]);
     }
-    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ",scores[i]);
+    }
     return 0;
 }
