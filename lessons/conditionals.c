cd750dd52358a0b50f3394016d1c364dd88d059e
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2026-02-03 09:23:36 +0000

    ternary operators

--- lessons/conditionals.c
+++ lessons/conditionals.c
@@ -13,5 +13,18 @@ int main()
     1. !  2. &&  3. ||
     */
 
-    return 0;
+
+    //Ternary operators ? = shorthand for if-else statements
+    // (condition)? value_if_true : value_if_false;
+     int number;
+     printf("Enter a number: ");
+     scanf("%d",&number);
+     printf("%d is an %s number\n",number, (number%2==0)? "Even": "Odd");
+     
+     int hours = 18;
+     int minutes = 19;
+     //02 gives the padding
+     
+     printf("It's %02d:%02d %s", hours, minutes,(hours < 12) ? "AM":"PM");
+     return 0;
 }
