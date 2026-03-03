f125bab61b64c096e7fa2f1c559beb341a65748d
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2026-03-03 13:17:32 +0000

    ECG_smart_meter

---
+++ Projects/functions_w_pointers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void poll_sensors(float *v_ptr, float*i_pr);
+float calculate_power(float*v_ptr, float*i_pr);
+float grid_voltage = 0.0;
+float load_current = 0.0;
+int main()
+{
+
+    float apparent_power = 0.0;
+
+    poll_sensors(&grid_voltage, &load_current);
+    apparent_power = calculate_power(&grid_voltage,&load_current);
+
+    printf("-----ELECTRICITY COMPANY OF GHANA DATA SHEET-----\n");
+    printf("Grid Voltage: %.2f V\nLine Current: %.2f A\n", grid_voltage, load_current);
+    printf("APPARENT POWER: %.2f VA", apparent_power);
+    return 0;
+}
+void poll_sensors(float *v_ptr, float *i_pr)
+{
+    printf("Enter the grid voltage: ");
+    scanf("%f", v_ptr);
+    printf("Enter the line current: ");
+    scanf("%f", i_pr);
+}
+float calculate_power(float*v_ptr, float*i_pr)
+{
+    return (*v_ptr) * (*i_pr);
+}
