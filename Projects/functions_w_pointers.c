75088158d9b517f81bf0e1e7ce5db25fd0ef18ce
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2026-03-04 11:50:33 +0000

    code_comments

--- Projects/functions_w_pointers.c
+++ Projects/functions_w_pointers.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 void poll_sensors(float *v_ptr, float*i_pr);
 float calculate_power(float*v_ptr, float*i_pr);
-float grid_voltage = 0.0;
-float load_current = 0.0;
+
+//GLOBAL VARIABLES TO BE USED IN FUNCTIONS
+float grid_voltage = 0.0f;
+float load_current = 0.0f;
 int main()
 {
 
-    float apparent_power = 0.0;
+    float apparent_power = 0.0f;
 
+// FUNCTION CALLS
     poll_sensors(&grid_voltage, &load_current);
     apparent_power = calculate_power(&grid_voltage,&load_current);
 
+//DISPLAY OF RESULTS
     printf("-----ELECTRICITY COMPANY OF GHANA DATA SHEET-----\n");
     printf("Grid Voltage: %.2f V\nLine Current: %.2f A\n", grid_voltage, load_current);
     printf("APPARENT POWER: %.2f VA", apparent_power);
     return 0;
 }
+// FUNCTION DECLARATIONS
 void poll_sensors(float *v_ptr, float *i_pr)
 {
     printf("Enter the grid voltage: ");
